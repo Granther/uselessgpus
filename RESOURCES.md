127e1eb4f2340222bf8d79ef24927c827378b0d7
Grant Herrema <granther321@gmail.com>
2024-08-19 15:14:13 -0400

    Full prototype pipeline is done, need to test with small LLM

---
+++ RESOURCES.md
@@ -0,0 +1,5 @@
+https://python.langchain.com/v0.1/docs/modules/data_connection/document_loaders/json/
+https://python.langchain.com/v0.2/docs/integrations/vectorstores/chroma/
+https://www.tutorialspoint.com/downloading-files-from-web-using-python
+https://github.com/dodeeric/json-files-ingestion-into-chroma-vector-db/blob/main/app.py
+https://docs.unstructured.io/api-reference/ingest/source-connectors/local?utm_medium=email&_hsenc=p2ANqtz-8tzN8AJX7w1oTE9GYKJHW94RaKUNOGBUg13ie6p3pyVTvzGLIDTHaBxj9_P8YyCrYe3m9hgy5B3cAFvRiEJa0YwM9LdA&_hsmi=320264015&utm_content=320264015&utm_source=hs_email
--- unst.py
+++ unst.py
@@ -1,58 +1,88 @@
-from unstructured_client import UnstructuredClient
-from unstructured_client.models import operations, shared
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
-
-# client = UnstructuredClient(
-#     api_key_auth="EbaLpESG9umX1surY6GXtCJBVVFRzU",
-#     server_url="https://api.unstructuredapp.io",
-# )
-
-# with open("hello.htm", "rb") as f:
-#     files = shared.Files(
-#         content=f.read(),
-#         file_name="hello.htm"
-#     )
-
-# req = operations.PartitionRequest(
-#     shared.PartitionParameters(
-#         files=files,
-#         strategy=shared.Strategy.HI_RES,
-#         chunking_strategy="basic",
-#         new_after_n_chars=200,
-#         max_characters=300
-#     )
-# )
-
-# print(req)
-
-# from unstructured.partition.html import partition_html
-# # Using partition_html to ingest HTML content
-# document_elements = partition_html(filename="hello.htm")
-
-# print(document_elements)
-# from unstructured.partition.html import partition_html
-
-# url = "https://www.cnn.com/2023/01/30/sport/empire-state-building-green-philadelphia-eagles-spt-intl/index.html"
-# elements = partition_html(filename="hello.htm")
-# print("\n\n".join([str(el) for el in elements]))
-
-# from unstructured.partition.api import partition_via_api
-
-# filename = "hello.htm"
-
-# elements = partition_via_api(
-#   filename=filename, api_key="EbaLpESG9umX1surY6GXtCJBVVFRzU", strategy="auto"
-# )
-
-# print(elements)
-
+from tavily import TavilyClient
 import requests
-from bs4 import BeautifulSoup
+from unstructured_ingest.connector.local import SimpleLocalConfig
+from unstructured_ingest.interfaces import PartitionConfig, ProcessorConfig, ReadConfig
+from unstructured_ingest.runner import LocalRunner
+from langchain_community.document_loaders import JSONLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+import chromadb
+from uuid import uuid4
+from pprint import pprint
+from langchain_huggingface import HuggingFaceEmbeddings
 
-htm = requests.get("https://beautiful-soup-4.readthedocs.io/en/latest/")
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-soup = BeautifulSoup(htm.text, "html.parser")
+search = "Tell me about golden retrievers"
+collection_name = "rags"
+file_path = "./local-ingest-output/pysocket.html.json"
+out_dir = "./out"
 
-print(soup.prettify())
+def query_col(query: str, collection_name: str, n_results: int = 1):
+    persistent_client = chromadb.PersistentClient()
+    collection = persistent_client.get_or_create_collection(collection_name)
 
-print(soup.title)
+    res = collection.query(query_texts=[query], n_results=n_results)
+    
+    return res
+
+def download_url(url: str, out_dir: str):
+    r = requests.get(url)
+    open(out_dir + "/" + str(uuid4()) + ".html", "wb").write(r.content)
+
+def partition(html_dir: str, out_dir: str):
+    runner = LocalRunner(
+        processor_config=ProcessorConfig(
+            verbose=True,
+            output_dir=out_dir,
+            num_processes=2,
+        ),
+        read_config=ReadConfig(),
+        partition_config=PartitionConfig(
+            partition_by_api=True,
+            api_key="EbaLpESG9umX1surY6GXtCJBVVFRzU",
+            partition_endpoint="https://api.unstructuredapp.io/general/v0/general",
+        ),
+        connector_config=SimpleLocalConfig(
+            input_path=html_dir,
+            recursive=True,
+        ),
+    )
+    runner.run()
+
+def embed_json(json_path: str, collection_name: str):
+    loader = JSONLoader(file_path=json_path, jq_schema=".[]", text_content=False)
+    documents = loader.load()
+    persistent_client = chromadb.PersistentClient()
+    collection = persistent_client.get_or_create_collection(collection_name)
+    langchain_chroma = Chroma(
+        client=persistent_client,
+        collection_name=collection_name,
+        embedding_function=embeddings
+    )
+
+    uuids = [str(uuid4()) for _ in range(len(documents))]
+
+    langchain_chroma.add_documents(documents=documents, ids=uuids)
+
+if __name__ == "__main__":
+    client = TavilyClient(api_key="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")
+    response = client.search(search, search_depth="basic", topic="general", max_results=1)
+
+    for item in response['results']:
+        download_url(item['url'], out_dir=out_dir)
+
+    partition(out_dir, "./partitioned")
+
+    for file in os.listdir("./partitioned"):
+        embed_json("./partitioned/" + file, "dogs")
+
+    print(query_col("How long do golden retrievers live", "dogs"))
+
+
+
